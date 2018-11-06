a683a38f414ef9e34079e471ce7851c03cb1dd08
Michael Singh <sing4346@c43182m.local>
2018-11-07 10:24:37 +1300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Michael Singh on 7/11/18.
+//  Copyright © 2018 Michael Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
